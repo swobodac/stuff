b1cd905fd37ace53f9df2320897f09bbcef340be
Conner <98852602+swobodac@users.noreply.github.com>
2025-12-02 16:40:50 -0500

    had to use the default penguinmod code:sob:

--- tryingtocodeanextenstionforpenguinmod.js
+++ tryingtocodeanextenstionforpenguinmod.js
@@ -1,26 +1,24 @@
-(function (Scratch)) {
-'use strict';
-  class ImLearningOK {
-    getInfo(){
-      return{
-        id: "ImLearningOK",
-        name: "My First Coded Extenstion",
-        docsURI: 'https://tenor.com/view/homers-guide-homer-homer-forehead-gif-8646990802834571000',
-        color1:"#F54927",
-        color2:"#FC826A",
-        color3:"#591000",
-        blocks:[
-  {
-    opcode: 'logToConsole',
-    text: 'log to console'
-  }
-]
+(function(Scratch) {
+  'use strict';
+  class Extension {
+    getInfo() {
+      return {
+        id: "johnMyExtension",
+        name: "My Extension",
+        blocks: [
+          {
+            opcode: 'logToConsole',
+            text: 'log to console',
+            blockType: Scratch.BlockType.COMMAND
+          }
+        ]
       };
     }
+
+    logToConsole() {
+      console.log('Hello world!');
+    }
   }
-    LogToConsole(){
-    console.log('homer, ding, homer, ding, homer, ding, homer.... ALERT ALERT');
-  }
-}
-  Scratch.extenstions.register(new ImLearningOK());
+
+  Scratch.extensions.register(new Extension());
 })(Scratch);
