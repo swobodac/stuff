8be9546f007bafb1ca5d1c3dff232045cd892a59
Conner <98852602+swobodac@users.noreply.github.com>
2025-12-02 16:54:58 -0500

    swtiching this to turbowarps extenstion guide

--- tryingtocodeanextenstionforpenguinmod.js
+++ tryingtocodeanextenstionforpenguinmod.js
@@ -1,46 +1,21 @@
-(function(Scratch) {
-  'use strict';
-  class Extension {
-    getInfo() {
-      return {
-        id: "johnMyExtension",
-        name: "My Extension",
-        blocks: [
-          {
-            opcode: 'logToConsole',
-            text: 'log [TEXT] to console',
-            blockType: Scratch.BlockType.COMMAND
-			  arguments: {
-		  TEXT:{}
-			  }
-          },
-          {
-            opcode: 'testReporter',
-            text: 'testing!',
-            blockType: Scratch.BlockType.REPORTER,
-            disableMonitor: true,
-	    allowDropAnywhere: true
-          },
-			          {
-            opcode: 'random',
-            text: 'randomize true & false yay',
-            blockType: Scratch.BlockType.BOOLEAN,
-			disableMonitor: true
-          }
-        ]
-      };
-    }
-
-    logToConsole() {
-      console.log('Hello world!');
-    }
-    testReporter() {
-      return "Hello world!";
-    }
-	      random() {
-      return Math.round(Math.random()) === 1;
-    }
+class HelloWorld {
+  getInfo() {
+    return {
+      id: 'helloworld',
+      name: 'It works!',
+      blocks: [
+        {
+          opcode: 'hello',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'Hello!'
+        }
+      ]
+    };
   }
 
-  Scratch.extensions.register(new Extension());
-})(Scratch);
+  hello() {
+    return 'World!';
+  }
+}
+
+Scratch.extensions.register(new HelloWorld());
