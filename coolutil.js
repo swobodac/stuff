560c5aa079f761a7a4e16df08d2e6175dc6f3c2c
Conner <98852602+swobodac@users.noreply.github.com>
2026-03-25 19:57:57 -0400

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- coolutil.js
+++ coolutil.js
@@ -1,5 +1,6 @@
 //ts has a lot of crap stolen from other pm extenstions like looks expanded, runtime, etc because i kinda suck at js:sob:
 //managed to figure out some functions using dinobuilder & other pm extenstions :)
+//iframe block
 
 (function(Scratch) {
   'use strict';
